3a44bd4b9457847e1f7f005b96d76fdef50efed1
Lance Gabriel L. Cruz <97740590+lancegabgab@users.noreply.github.com>
2026-01-20 22:45:20 +0800

    Update index.js
    used the env variables

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//required node modules
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/users");
@@ -6,9 +6,7 @@ const productRoutes = require("./routes/products");
 const cartRoutes = require("./routes/cart");
 const orderRoutes = require("./routes/order");
 
-
-//port
-const port = 4000;
+const port = process.env.PORT;
 
 const cors = require("cors");
 const app = express();
@@ -18,16 +16,14 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-//database connection
-mongoose.connect("mongodb+srv://admin:admin@lancegabgab.hdzgoli.mongodb.net/CAPSTONE_ShopeeYan", 
+mongoose.connect(process.env.CONN_STRING, 
 	{
-		useNewUrlParser: true, // For parsing/reading connection string
-		useUnifiedTopology: true	// Assures that our application uses mongodb latest servers when connecting with mongo database
+		useNewUrlParser: true,
+		useUnifiedTopology: true
 	});
 mongoose.connection.once('open', () => console.log('Now connected to MongoDB Atlas'));
 
 //for Deployed in AWS routes
-
 app.get("b4", (req, res) => {
 res.send("Hello world")
 })
@@ -36,15 +32,6 @@ app.use("/users", userRoutes);
 app.use("/products", productRoutes);
 app.use("/cart", cartRoutes);
 app.use("/order", orderRoutes);
-
-
-//this is the routes for testing the not Deployed in AWS version
-/*
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
-app.use("/cart", cartRoutes);
-app.use("/order", orderRoutes);
-*/
 
 app.listen(port, () => {
 	console.log(`Port is running at ${port}`)
